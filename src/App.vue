10610b644ab656c92e0cd769684df12707f564c4
kasszym <s200893@sggw.edu.pl>
2025-11-05 14:54:51 +0100

    feat(): add promo banner

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import Header from "./components/Header.vue";
 import IntroComponent from "./components/MainPageComponents/IntroComponent.vue";
 import CarRanges from "./components/CarRange.vue/CarRanges.vue";
+import PromoBanner from "./components/PromoBanner.vue";
 </script>
 
 <template>
@@ -12,8 +13,8 @@ import CarRanges from "./components/CarRange.vue/CarRanges.vue";
       style="gap: 40px; margin: 32px auto"
     >
       <IntroComponent />
-      <div><CarRanges /></div>
-      <div><p>sekcja 3</p></div>
+      <CarRanges />
+      <PromoBanner />
     </main>
   </div>
 </template>
---
+++ src/components/PromoBanner.vue
@@ -0,0 +1,32 @@
+<script setup>
+import ButtonComponent from "./common/ButtonComponent.vue";
+</script>
+
+<template>
+  <div
+    class="promo-banner d-flex justify-content-between align-items-center p-4 text-white"
+  >
+    <span
+      class="fw-bold"
+      style="font-size: 22px"
+      >Poczuj przyszłość jazdy z Voltura.</span
+    >
+
+    <ButtonComponent
+      title="Umów się na jazdę próbną"
+      width="256px"
+      background-color="#fff"
+      color="var(--blue)"
+      class="me-2"
+      font-size="var(--fs-s)"
+    />
+  </div>
+</template>
+
+<style scoped>
+.promo-banner {
+  background: var(--blue);
+  border-radius: var(--border-radius);
+  height: 120px;
+}
+</style>
